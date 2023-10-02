716f8e8443596aabcb517a6c9e210bb6c4a3a8ff
skrewde <94148640+skrewde@users.noreply.github.com>
2023-10-02 21:57:14 +0100

    Update rock.py
    * Create scoreline funtion
    * Make user input valid regardless of case

--- ch3/rock.py
+++ ch3/rock.py
@@ -1,10 +1,11 @@
-# Rock, Paper, Scissors game in Python
-
-# Import random module for use later
 import random
+import time
 
 # Assign the variable winner to an empty string, to be manipulated later
 winner = ''
+
+user_wins = 0
+computer_wins = 0
 
 # Initial random choice generator (feat. some tests); Lengthy, less readable, & convoluted
     #computer_choice = random.randint(0, 2)
@@ -23,38 +24,51 @@ winner = ''
 # A list containing the possible choices
 choices = ['rock', 'paper', 'scissors']
 
-# Pick a random choice using the random.choice() function
-computer_choice = random.choice(choices)
+
 
 # user_choice = input('rock, paper, or scissors? \n')
-
-# Prompt user for their choice. 
 user_choice = ''
-while (user_choice != 'rock'
-and user_choice != 'paper'
-and user_choice != 'scissors'):
-    user_choice = input('rock, paper or scissors? \n')
+computer_choice = ''
 
-# Logic for determining the winner
-if computer_choice == user_choice:
-    winner = 'Tie'
-elif computer_choice == 'rock' and user_choice == 'scissors':
-    winner = 'Computer'
-elif computer_choice == 'paper' and user_choice == 'rock':
-    winner = 'Computer'
-elif computer_choice == 'scissors' and user_choice == 'paper':
-    winner = 'Computer'
-else:
-    winner = 'User'
 
-# Logic for user friendly output
-if winner == 'Tie':
-    print('The computer also chose '+ computer_choice+', it\'s a tie :0')
-elif winner == 'Computer':
-    print('The computer chose '+ computer_choice+', You lost :(')
-else:
-    print('The computer chose '+ computer_choice+', You won!')
+while (user_choice != 'rock' and user_choice != 'paper' and user_choice != 'scissors'):
+    user_choice = input('rock, paper or scissors? \n').lower()
+
+    # Pick a random choice using the random module
+    computer_choice = random.choice(choices)
+
+    # Logic for determining the winner
+    if computer_choice == user_choice:
+        winner = 'Tie'
+    elif computer_choice == 'rock' and user_choice == 'scissors':
+        winner = 'Computer'
+        computer_wins += 1
+    elif computer_choice == 'paper' and user_choice == 'rock':
+        winner = 'Computer'
+        computer_wins += 1
+    elif computer_choice == 'scissors' and user_choice == 'paper':
+        winner = 'Computer'
+        computer_wins += 1
+    else:
+        winner = 'User'
+        computer_wins += 1
+
+    # Logic for user friendly output
+    if winner == 'Tie':
+        print('The computer also chose '+ computer_choice+', it\'s a tie :0')
+    elif winner == 'Computer':
+        print('The computer chose '+ computer_choice+', You lost :(')
+    else:
+        print('The computer chose '+ computer_choice+', You won!')
+
+
+def get_scores():
+    print("You won", user_wins, "times.")
+    print("The computer won", computer_wins, "times.")
+    print("Goodbye!")
 
 ## TO_DO!
-#1 Create a scoreline function that saves the scores even when the program exits
-#2 Make all permutations of user's choice valid (e.g. All cases of 'rock' like 'Rock' or 'rOCk')
+#1 Create a scoreline function that saves the scores even when the program exits. DONE.
+#2 Make all permutations of user's choice valid (e.g. All cases of 'rock' like 'Rock' or 'rOCk'). DONE.
+#3 Create user input for scoreline function
+#4 Make program run indefinitely till user quits
